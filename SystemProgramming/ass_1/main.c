46c48561f2e58c5ae62f05de9632553adb6023f0
ash <sehyeon014@gmail.com>
2022-03-25 14:40:40 +0900

    program_assignment_1

--- SystemProgramming/ass_1/main.c
+++ SystemProgramming/ass_1/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
+
+void print_opt_list()
+{
+	printf("-p : \"Welcome Linux Programming!\"\n");
+	printf("-n [argument] : \"Nice to meet [argument]\"\n");
+	printf("-h : Print Option List\n");
+}
 
 int main(int argc, char **argv)
 {
-	if (argc > 2)
-		printf("-p\n-n\n-h\n");
+	int op;
+
+	if (argc < 2)
+		print_opt_list();
+	op = getopt(argc, argv, "phn:");
+	switch(op)
+	{
+		case 'p':
+			printf("Welcome Linux Programming!\n");
+			break;
+		case 'h':
+			print_opt_list();
+			break;
+		case 'n':
+			printf("Nice to meet %s\n", argv[2]);
+			break;
+		case '?':
+			printf("Wrong Option\n");
+			break;
+	}
 	return (0);
 }
