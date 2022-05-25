855ba928f115ce9593eae5a86be0ce01ca254050
ash <sehyeon014@gmail.com>
2022-05-26 03:18:44 +0900

    merge

--- SystemProgramming/hw3/hw3.c
+++ SystemProgramming/hw3/hw3.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
-#include <time.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <time.h>
 
 int main(int argc, char **argv)
 {
-	char *t;
+	FILE *fp;
+
 	if (argc != 2) {
 		fprintf(stderr, "Only 1 argument please!\n");
-		return (0);
+		return (1);
 	}
-	// FILE *fp;
-	// if ((fp = fopen(argv[1],O_RDWR) == -1)) {
-	// 	fprintf(stderr, "Failed to open\n");
-	// 	exit(1);
-	// }
-	t = ctime(time(NULL));
-	printf("%s\n", t);
+	if ((fp = fopen(argv[1], "r")) == NULL) {
+		perror("Failed to open!");
+		return (1);
+	}
+	long buf;
+	char *stime;
+	fscanf(fp, "%ld", &buf);
+	stime = ctime(&buf);
+	printf("%s", stime);
 	return (0);
 }
